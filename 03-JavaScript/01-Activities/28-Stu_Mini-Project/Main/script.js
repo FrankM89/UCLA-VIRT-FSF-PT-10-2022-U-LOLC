fd3e5e5d63caade0611311d020e72dabea72a755
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2022-10-24 20:50:37 -0800

    rps game

--- 03-JavaScript/01-Activities/28-Stu_Mini-Project/Main/script.js
+++ 03-JavaScript/01-Activities/28-Stu_Mini-Project/Main/script.js
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-// Rock, Paper, Scissor game :o
 
-=======
->>>>>>> 0624b1aed89a7d55526f3fabd197da86da1dd700
-var wins = 0;
-var ties = 0;
-var losses = 0;
-
-<<<<<<< HEAD
-// Figure out the user's choice
-var choice = prompt("Enter R, P or S");
-
-// Figure out the computer's choice
-var choices = ["R", "P", "S"];
-var Index = Math.floor(Math.random() * choices.length);
-
-var computerChoice = choices[Index];
-
-
-if (userChoice === "R" && computerChoice === "S") {
-    alert("You win!");
-} else if(userChoice === "R" && computerChoice === "P") {
-    alert("You lose!");
-};
-
-if (userChoice === "S" && computerChoice === "P") {
-    alert("You win!");
-} else if()
-=======
 // Array of options for computer to pick from
 var options = ["R", "P", "S"];
 
@@ -85,4 +56,4 @@ var playGame = function() {
 
 // Run the game for the first time
 playGame();
->>>>>>> 0624b1aed89a7d55526f3fabd197da86da1dd700
+
