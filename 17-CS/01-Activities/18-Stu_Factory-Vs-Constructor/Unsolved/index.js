142be466d8617684d0f66d4f91b2f40d0382f6ac
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2023-02-13 21:32:36 -0800

    Finish up CS

--- 17-CS/01-Activities/18-Stu_Factory-Vs-Constructor/Unsolved/index.js
+++ 17-CS/01-Activities/18-Stu_Factory-Vs-Constructor/Unsolved/index.js
@@ -1,13 +1,61 @@
 // TODO: Refactor the class to a factory function.
-class Lesson {
-  constructor() {
-    this.title = 'Module 17 - Computer Science';
-    this.description = 'CS for JS';
-  }
-  information() {
-    console.log(this.title, this.description);
-  }
-}
+// class Lesson {
+//   constructor() {
+//     this.title = 'Module 17 - Computer Science';
+//     this.description = 'CS for JS';
+//   }
+//   information() {
+//     console.log(this.title, this.description);
+//   }
+// }
 
-const csForJS = new Lesson();
+// const csForJS = new Lesson();
+// csForJS.information();
+
+
+
+//  Maria's refactored version  \\
+
+const createLesson = () => {
+  title = "Module 17 - Computer Science";
+  description = "CS for JS";
+
+  return {
+    information: () => console.log(title, description),
+  };
+};
+
+const csForJS = createLesson();
 csForJS.information();
+
+
+
+//          CHATGPT CAME UP WITH THESE            \\
+
+// function createLesson() {
+//   const title = 'Module 17 - Computer Science';
+//   const description = 'CS for JS';
+
+//   return {
+//     information: function() {
+//       console.log(title, description);
+//     }
+//   };
+// }
+
+// const csForJS = createLesson();
+// csForJS.information();
+
+// function createLesson() {
+//   const title = 'Module 17 - Computer Science';
+//   const description = 'CS for JS';
+
+//   function information() {
+//     console.log(title, description);
+//   }
+
+//   return { information };
+// }
+
+// const csForJS = createLesson();
+// csForJS.information();
--- 17-CS/01-Activities/20-Stu_Event-Delegation-Handling/Unsolved/assets/js/script.js
+++ 17-CS/01-Activities/20-Stu_Event-Delegation-Handling/Unsolved/assets/js/script.js
@@ -2,19 +2,25 @@ const buttons = document.getElementsByTagName('button');
 
 const clickHandler = function () {
 // TODO: Add a comment describing the purpose of this variable.
+// On page load, start the count iterance at 0
   let count = 0;
 
   // TODO: Add a comment describing what is happening. 
+  // Increment function called (seperately) on each button element
   return function () {
   
    // TODO: Add a comment describing how this variable is being scoped. 
+   // Increment the count value (number) by 1 each event
     count++;
    // TODO: Add a comment describing how we are using the 'count' variable.
+   // This element replaces the inner-text with the updated language where count refers back to the count iteration
     this.textContent = `Clicks: ${count}`;
   };
 };
 
 // TODO: Add a comment describing the purpose of this loop.
+// For loop starting at inital for all buttons
+// For this button on each click run the clickhandler function
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', clickHandler());
 }
