e6e2632fabba0691b1b4bfddf2840bc539db313e
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2023-03-09 21:20:56 -0800

    Start MERN

--- 21-MERN/01-Activities/02-Stu_Apollo-Server/Unsolved/schemas/typeDefs.js
+++ 21-MERN/01-Activities/02-Stu_Apollo-Server/Unsolved/schemas/typeDefs.js
@@ -2,6 +2,7 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   # TODO: Add a comment describing the functionality of this statement
+  # Define which fields are accessible from the Class model
   type Class {
     _id: ID
     name: String
@@ -10,6 +11,7 @@ const typeDefs = gql`
   }
 
   # TODO: Add a comment describing the functionality of this statement
+  # Query for array of class in DB
   type Query {
     classes: [Class]
   }
--- 21-MERN/01-Activities/02-Stu_Apollo-Server/Unsolved/server.js
+++ 21-MERN/01-Activities/02-Stu_Apollo-Server/Unsolved/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 // TODO: Add a comment describing the functionality of this expression
+// Importing Apollo server express
 const { ApolloServer } = require('apollo-server-express');
 
 // TODO: Add a comment describing the functionality of this expression
+// Importing schemas files for db
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -18,6 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // TODO: Add a comment describing the functionality of this async function
+// This starts the server and applied middleware
+// Creates a new instance of an Apollo serer with GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
 await server.start();
 server.applyMiddleware({ app });
@@ -31,4 +35,5 @@ db.once('open', () => {
 };
 
 // TODO: Add a comment describing this functionality
+// Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
